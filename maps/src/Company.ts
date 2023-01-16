36a7b408b8f6957c9677013f3b534d5ed8a31f8b
CarolinnaCiocanu <92712560+CarolinnaCiocanu@users.noreply.github.com>
2023-01-16 10:49:06 +0200

    Added Markers to Map.

--- maps/src/Company.ts
+++ maps/src/Company.ts
@@ -1,12 +1,14 @@
+/// <reference types="@types/google.maps" />
 import { faker } from "@faker-js/faker";
-
-export class Company {
+import { Mappable } from './CustomMap';
+export class Company implements Mappable {
   companyName: string;
   catchPhrase: string;
   location: {
     lat: number;
     lng: number;
   };
+  color: string = 'red';
 
   constructor() {
     this.companyName = faker.company.companyName();
@@ -15,5 +17,14 @@ export class Company {
       lat: parseFloat(faker.address.latitude()),
       lng: parseFloat(faker.address.longitude()),
     };
+  }
+
+  markerContent(): string {
+    return `
+      <div>
+        <h1>Company Name: ${this.companyName}</h1>
+        <h3>Catchphrase: ${this.catchPhrase}</h3>
+      </div>
+    `;
   }
 }

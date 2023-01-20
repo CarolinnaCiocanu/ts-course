531a8dfb074c688f7a82e798f678bf4662cf6b05
CarolinnaCiocanu <92712560+CarolinnaCiocanu@users.noreply.github.com>
2023-01-20 16:15:48 +0200

    Updates for web project.

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,7 +1,7 @@
 import { User } from "./models/User";
 
-const user = new User({ id: 1 });
+const user = new User({ name: 'new record', age: 0 });
 
-user.set({ name: 'NEW NAME', age: 23 });
+// user.set({ name: 'NEW NAME', age: 93 });
 
 user.save();
--- web/src/models/User.ts
+++ web/src/models/User.ts
@@ -1,59 +1,36 @@
-import axios, { AxiosResponse } from 'axios';
-
+import axios, { AxiosResponse } from "axios";
 interface UserProps {
-    id?: number;
-    name?: string;
-    age?: number;
+  id?: number;
+  name?: string;
+  age?: number;
 }
 
-type Callback = () => void;
-
 export class User {
-    events: { [key: string]: Callback[] } = {};
+  constructor(private data: UserProps) {}
 
-    constructor(private data: UserProps) { }
+  get(propName: string): number | string {
+    return this.data[propName];
+  }
 
-    get(propName: string): (number | string) {
-        return this.data[propName];
+  set(update: UserProps): void {
+    Object.assign(this.data, update);
+  }
+
+  fetch(): void {
+    axios
+      .get(`http://localhost:3000/users/${this.get("id")}`)
+      .then((response: AxiosResponse): void => {
+        this.set(response.data);
+      });
+  }
+
+  save(): void {
+    const id = this.get("id");
+
+    if (id) {
+      axios.put(`http://localhost:3000/users/${id}`, this.data);
+    } else {
+      axios.post("http://localhost:3000/users", this.data);
     }
-
-    set(update: UserProps): void {
-        Object.assign(this.data, update);
-    }
-
-    on(eventName: string, callBack: Callback): void {
-        const handlers = this.events[eventName] || [];
-
-        handlers.push(callBack);
-
-        this.events[eventName] = handlers;
-    }
-
-    trigger(eventName: string): void {
-        const handlers = this.events[eventName];
-
-        if (!handlers || handlers.length === 0) {
-            return;
-        }
-
-        handlers.forEach(callback => {
-            callback();
-        });
-    }
-
-    fetch(): void {
-        axios.get(`http://localhost:3000/users/${this.get('id')}`).then((response: AxiosResponse): void => {
-            this.set(response.data);
-        });
-    }
-
-    save(): void {
-        const id = this.get('id');
-
-        if (id) {
-            axios.put(`http://localhost:3000/users/${id}`, this.data);
-        } else {
-            axios.post('http://localhost:3000/users', this.data);
-        }
-    }
+  }
 }
